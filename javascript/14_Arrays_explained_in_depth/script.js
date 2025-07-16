d1628b9f2bf35a57e0af9d49ae6018c25424866c
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:52:10 +0500

    Add files via upload

---
+++ javascript/14_Arrays_explained_in_depth/script.js
@@ -0,0 +1,10 @@
+const fruitsCollection=['Apples','Banana','Grapes',true,23,false,undefined,null]
+// console.log(fruitsCollection)
+fruitsCollection[fruitsCollection.length]='sabzi'
+fruitsCollection.push('Kaddu')
+console.log(fruitsCollection)
+fruitsCollection.push('ladyfinger',354,true,undefined,null)
+console.log(fruitsCollection)
+fruitsCollection.pop()
+const newObject=[]
+newObject.firstName='zahid'
