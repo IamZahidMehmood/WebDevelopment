2bc1bf87b0969fd1de98b8a88244b87c184ba0de
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:52:31 +0500

    Add files via upload

---
+++ javascript/15_Array_methods/script.js
@@ -0,0 +1,18 @@
+const evenNumbers=[0,2,4,6,8,10]
+// evenNumbers.shift()//removes first number from start
+// evenNumbers.shift()
+// evenNumbers.unshift(4)
+const oddNumbers=[1,3,5,7,9]
+const fruitsArr=['banana','apples','Dates']
+const animals=['cat','rat','dog','cheta','goat','cow','tiger']
+
+console.log(evenNumbers.concat(oddNumbers))
+const addedArray1=evenNumbers.concat(oddNumbers,fruitsArr)
+const addedArray2=oddNumbers.concat(evenNumbers)
+oddNumbers.indexOf(5)
+fruitsArr.reverse()
+// animals.sort()
+// animals.slice(startvalue,endvalue)
+// slice original array ko koi harm nehi krta aik new array bna kr deta hai
+//splice new array deta but original ko b change kr deta
+// animals.splice(2,2,'kutta','bhuli')
