5e0a0ac4d95ada816539109766a9b15c7f16dcc2
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:54:42 +0500

    Add files via upload

---
+++ javascript/20_do_while_loop/script.js
@@ -0,0 +1,17 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// const friends=['zahid','tahir','shahid','Hassam','AbuBakr','Musa','Sultan']
+// for(let i=0;i<friends.length;i++){
+//     console.log(friends[i])
+// }
+// for(let i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i,' is even number')
+//     }
+// }
+let i=1
+do{
+    console.log(i)
+    i++
+}while(i<4)
