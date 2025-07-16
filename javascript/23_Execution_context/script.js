21ca2e7c68aee268f8249436397e67443bad368b
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:55:49 +0500

    Add files via upload

---
+++ javascript/23_Execution_context/script.js
@@ -0,0 +1,13 @@
+debugger
+sayHi()
+const username='zahid'
+console.log(username)
+//Global Execution phase
+//there are two types of Global execution phase
+// 1.memory creation phase
+// 2.code execution phase
+function sayHi(){
+    const a=14
+    const b=12
+    console.log(a,b)
+}
