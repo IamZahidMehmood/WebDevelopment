dce6d140a0a944b40bacbfdd2c5b8ca5e71717ee
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:55:26 +0500

    Add files via upload

---
+++ javascript/22_Return_keyword/script.js
@@ -0,0 +1,21 @@
+// function addTwoNumbers(){
+//     let a=2
+//     let b=5
+//     let c=a+b
+//     return c
+// }
+// let result=addTwoNumbers()
+// console.log(result)
+
+// function addTwoNumbers(){
+//     return 9+1
+// }
+// let result=addTwoNumbers()
+// console.log(result)
+
+function addTwoNumbers(a,b){
+    let c=a+b
+    return c
+}
+let result=addTwoNumbers(addTwoNumbers(2,5),addTwoNumbers(2,5))
+console.log(result)
