7221a30efcf5bf9ddc039d49e92f9fc658b15756
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:52:51 +0500

    Add files via upload

---
+++ javascript/16_Multidimensional_arrays/script.js
@@ -0,0 +1,9 @@
+const nameAndNumber=[['zahid',77],['Tahir',79]];
+//how to access elements in two dimensional arrays
+// nameAndNumber[0][1] it will give 77 
+nameAndNumber[1][5]='usman';
+const ticTacToe=[
+    ['X',null,null],
+    [null,null,'O'],
+    ['O',null,'X']
+];
