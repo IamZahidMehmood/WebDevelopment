5c2461f402c41f05da0674cb95a882a130c05115
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:15:21 +0500

    Add files via upload

---
+++ javascript/00_hello_world/script.js
@@ -0,0 +1,12 @@
+console.log(lastName)
+var userName="Tahir"
+let lastName="Raja"
+let age=34
+userName=100
+age = 15
+let a
+a="hello"
+// console.log(lastName)
+// const hoursInDay
+let userIntro="Hi i am " + userName +" "+ lastName + " "+  "and i am " + " " + age + " " + "year old";
+let isHappy=true;
