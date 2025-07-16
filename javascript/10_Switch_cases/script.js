4e6b4434ffc16759cc84ba44ce9aa51c26ac6e92
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:23:03 +0500

    Add files via upload

---
+++ javascript/10_Switch_cases/script.js
@@ -0,0 +1,18 @@
+const num=3
+switch(num){
+    case 0:
+        console.log("Monday")
+        break
+    case 1:
+        console.log("Tuesday")
+        break
+    case 2:
+        console.log("Wednesday")
+        break
+    case 3:
+        console.log("Thursday")
+        break
+    default:
+        console.log("enter a valid day")
+        break
+}
