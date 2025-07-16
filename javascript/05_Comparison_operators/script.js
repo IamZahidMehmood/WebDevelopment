455ed827dceb2f22c486e78f8371cdf1bff55210
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:17:57 +0500

    Add files via upload

---
+++ javascript/05_Comparison_operators/script.js
@@ -0,0 +1,5 @@
+// ==,!=,<,>,<=,>=
+const userAge1='21'
+const userAge2=21
+// ==, === in == it will convert them into same datatype and then compare there values
+// === this does not convert there data types , there data types as well values must be the same for true output
