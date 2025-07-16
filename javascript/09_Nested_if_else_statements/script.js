08bc576dfeb35036bccadc6b80c099b95256117c
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:19:20 +0500

    Add files via upload

---
+++ javascript/09_Nested_if_else_statements/script.js
@@ -0,0 +1,18 @@
+let userName=prompt("Enter your name: ")
+let userAge=parseInt(prompt("Enter your age: "))
+if(userAge >= 0 && userAge <= 5){
+    console.log(`${userName} is a child`)
+    console.log("and he/she always play")
+}
+else if(userAge >= 6 && userAge <=19){
+    console.log(`${userName} is a college going teenage`)
+    console.log("and he/she is college going.")
+    if(userAge>=18){
+        console.log("user can apply for driving license")
+    }else{
+        console.log("user is under 18")
+    }
+}else{
+    console.log(`${userName} is a professional`)
+    console.log(" and he/she is doing a job");
+}
