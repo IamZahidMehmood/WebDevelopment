deb4049c7435b20db540237bf56b0034052dcacb
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:16:27 +0500

    Add files via upload

---
+++ javascript/02_template_literals(string methods&properties)/script.js
@@ -0,0 +1,8 @@
+// const userName='2489354'
+// const hell=userName.padEnd(12,'*')
+const myName = "  my name is zahid   "
+console.log(myName.replaceAll('m','t'))
+console.log(myName)
+console.log(myName.trim())
+// const myAccountNum='5743'
+// const templateString=`Last four digit of  my account number is ${myAccountNum.padStart(16,'*')}`
