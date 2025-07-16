c3aceff9375e05922f53dc6293d22ce336fc2fc0
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:18:17 +0500

    Add files via upload

---
+++ javascript/06_Logical_operators/script.js
@@ -0,0 +1,15 @@
+//  &&(AND), ||(OR), !(NOT)
+// const val1=33
+// const val2=12
+// const val3=15
+// 2 && 1 gives us =1
+// 0 && 3 gives =0
+// Boolean(2&&1)=true
+//Boolean(0&&3)=false
+// 2 || 1 gives us =2
+// 0 || 3 gives =0
+// const orResult= ''||'Hello'//result='Hello'
+// const andResult= ''&&'Hello'//result=''
+// or ko pehle hi agar true mil gya to aage nehi jae ga
+// and ko agar pehle hi false mil gya to aage nehi jae ga
+console.log(true && false || true)
