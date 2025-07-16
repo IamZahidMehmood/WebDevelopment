cab87ce5a4cc3efe78efb6024ceee5efdaa6ec79
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:59:04 +0500

    Add files via upload

---
+++ javascript/32_difference_bw_methods_and_functions/script.js
@@ -0,0 +1,16 @@
+// hr function method nehi hota but hr method function hota hai
+// aisa koi b function jise hum dot kr k call krte hain ya use krte hain, method kehlaata hai
+//jab aik function ko object ke andar rakh diya jaata hai to usse method bolte hain
+
+const maths={
+    E:2.718281828459045,
+    add:function(a,b){
+        return a+b
+    },
+    square(num){
+        return num*num
+    },
+    cube(num){
+        return num**num
+    }
+}
