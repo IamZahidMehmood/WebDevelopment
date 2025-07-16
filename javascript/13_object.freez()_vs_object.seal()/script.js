cedfdbdb86f8a49b616ac35b3bc91a1ae1c05cdf
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:51:50 +0500

    Add files via upload

---
+++ javascript/13_object.freez()_vs_object.seal()/script.js
@@ -0,0 +1,25 @@
+// const user='Zahid1'
+// const user3='Zahid1'
+// const userAge1=15
+// const userAge2=15
+// const isGraduate=false
+// const hasJob=false
+// const user1={}
+// const user2={}
+// const username1="hi"
+// const username2=""
+const myName='zahid'
+const user1={
+    'first-Name':'zahid'
+}
+const user2={
+    'first-Name':"tahir",
+    lastname:"mehmood",
+    zahid:"developer",
+    address:{
+        city:'rwp',
+        pinCode:34355,
+        state:"punjab"
+    }
+}
+let userName='Anurag'
