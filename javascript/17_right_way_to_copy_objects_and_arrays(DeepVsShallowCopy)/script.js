bb6e6756bdb7fb44c38eb3fc66c97fd72874f958
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:53:11 +0500

    Add files via upload

---
+++ javascript/17_right_way_to_copy_objects_and_arrays(DeepVsShallowCopy)/script.js
@@ -0,0 +1,40 @@
+const fruits=['Mangos','Apples','Banana']
+// const myFruits=fruits.slice()
+// const myFruits=fruits
+// myFruits.push('Grapes')
+// const myFruits=[]
+// Object.assign(myFruits,fruits)
+// const myFruits=[...fruits]
+// const myFruits=[].concat(fruits)
+// myFruits.push('Grapes')
+
+// Object.assign(myFruits,fruits)
+// myFruits.push('Grapes')
+// const myFruits=fruits
+// myFruits.push('Dates')
+// myFruits.push('Grapes')
+
+const user1={
+    firstName:'zahid',
+    lastName:'mehmood',
+    pata: {
+        city:'Rwp',
+        pincode:454444,
+    },
+    subject:['physics','chemistry','cs'],
+}
+const user2={...user1}
+// Object.assign(user2,user1)
+
+// const user2=user1
+// user2.lastName='Raja'
+// const user2={}
+// Object.assign(user2,user1)
+
+// const user2={...user1} //spread operator
+// user2.firstName='Tahir'
+// const username1='zahid'
+// const username2=username1
+// username2=username2 + ' mehmood'
+
+
