3bc2803e8dbf52cb8bf966d5e925ae8dd5026ffb
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:56:49 +0500

    Add files via upload

---
+++ javascript/26_Global_scope_vs_local_scope/script.js
@@ -0,0 +1,23 @@
+debugger
+const username='Zahid'
+let userAge=34
+var a=12
+
+function add(){
+    const x=5
+    const y=8
+    console.log(x+y)
+    console.log(username)
+}
+function subtract(){
+    const x=5
+    const y=8
+    console.log(x+y)
+    console.log(username)
+}
+//the variables jo k let const k through bnaae jaate wo scope script main jaate, and jo var and function hote wo window object main jaate hain
+add()
+subtract()
+console.log(username)
+console.log('Program Ended')
+// console.log(y)
