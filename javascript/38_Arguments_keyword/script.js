3a4ddf8c3996e72411418cf002fbf5521edd725e
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 13:01:15 +0500

    Add files via upload

---
+++ javascript/38_Arguments_keyword/script.js
@@ -0,0 +1,17 @@
+// function add(a,b){
+//     let sum=0
+//     for(let i=0;i<arguments.length;i++){
+//         sum += arguments[i]
+//     }
+//     return sum
+//     // return a+b
+// }
+
+const add=function (){
+    let sum=0
+    for(let i=0;i<arguments.length;i++){
+        sum += arguments[i]
+    }
+    return sum
+    // return a+b
+}
