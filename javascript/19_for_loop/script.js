76c27c05d40f430ff11172738830aff1333ce523
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:54:18 +0500

    Add files via upload

---
+++ javascript/19_for_loop/script.js
@@ -0,0 +1,12 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// const friends=['zahid','tahir','shahid','Hassam','AbuBakr','Musa','Sultan']
+// for(let i=0;i<friends.length;i++){
+//     console.log(friends[i])
+// }
+for(let i=0;i<=100;i++){
+    if(i%2==0){
+        console.log(i,' is even number')
+    }
+}
