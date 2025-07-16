b7d2374d5a2c760dc4668d15de3355a699c92846
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-07-16 12:51:28 +0500

    Add files via upload

---
+++ javascript/12_how_to_see_variable_address_in_dev_tools/script.js
@@ -0,0 +1,27 @@
+// const user='Zahid1'
+// const user3='Zahid1'
+// const userAge1=15
+// const userAge2=15
+// const isGraduate=false
+// const hasJob=false
+// const user1={}
+// const user2={}
+// const username1="hi"
+// const username2=""
+const myName='zahid'
+const user1={
+    'first-Name':'zahid'
+}
+const user2={
+    'first-Name':"tahir",
+    lastname:"mehmood",
+    zahid:"developer",
+    address:{
+        city:'rwp',
+        pinCode:34355,
+        state:"punjab"
+    }
+}
+// console.log(user2[myName])
+user2.age=23;
+user2.user_profession='teacher';
